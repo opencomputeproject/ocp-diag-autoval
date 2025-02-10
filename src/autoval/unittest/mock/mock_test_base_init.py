3e8e67c1894aa4eb4f2f754c229b30ea0f014d5b
Tristen Glover <tristenglov@meta.com>
2025-02-10 09:34:29 -0700

    [autoval-oss] Import unittests from autoval internal repo

---
+++ src/autoval/unittest/mock/mock_test_base_init.py
@@ -0,0 +1,8 @@
+from unittest.mock import patch
+
+from autoval.lib.test_args import TEST_CONTROL
+
+
+@patch.dict(TEST_CONTROL, {})
+def mock_testbase_init(self, *_):
+    self.test_control = TEST_CONTROL
